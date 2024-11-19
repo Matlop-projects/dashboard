75239f39b94baa93f1b4bfb42cfa66335403cf73
Ahmed Essam <40560225+iRashwan@users.noreply.github.com>
2024-11-19 05:39:58 +0200

    login module

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { ButtonModule } from 'primeng/button';
+import { NgIf } from '@angular/common';
+import { ToasterService } from '../../services/toaster.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [NgIf, ReactiveFormsModule , InputTextModule , PasswordModule ,ButtonModule ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  loginForm: FormGroup;
+  toaster = inject(ToasterService);
+
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required]],
+      password: ['', [Validators.required]]
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log('Form Submitted', this.loginForm.value);
+      this.onLogin(this.loginForm.value);
+    } else {
+      this.toaster.errorToaster('Please Complete All Feilds');
+    }
+  }
+
+  onLogin(loginfrom:any) {}
 
 }
