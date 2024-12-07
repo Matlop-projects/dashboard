3b24a660d4d2d295210c0bb594e2ca30cdcc7eb2
Ahmed Essam <40560225+iRashwan@users.noreply.github.com>
2024-12-07 06:11:54 +0200

    complete the two modules

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,74 +1,59 @@
-import { NgFor, NgIf } from '@angular/common';
-import { Component} from '@angular/core';
+import { NgFor } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { ApiService } from '../../services/api.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [NgIf,NgFor],
+  imports: [ NgFor , RouterModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
-  items:any=[
-   {
-     name:'عدد العملاء',
-     value:' 80',
-     img:'assets/images/cr-1.PNG'
-   },
-   {
-    name:'عدد موظفي الادارة',
-    value:' 17',
-    img:'assets/images/cr-2.PNG'
-  },
-  {
-    name:'عدد مقدمي الخدمة',
-    value:' 69',
-    img:'assets/images/cr-3.PNG'
-  },
-  {
-    name:'عدد الدول',
-    value:' 2',
-    img:'assets/images/cr-1.PNG'
-  },
-  {
-    name:'عدد المدن',
-    value:' 25',
-    img:'assets/images/cr-4.PNG'
-  },
-  {
-   name:'عدد الاقسام',
-   value:' 50',
-   img:'assets/images/cr-2.PNG'
- },
- {
-   name:'عدد الطلبات',
-   value:' 16',
-   img:'assets/images/cr-3.PNG'
- },
- {
-   name:' اجمالي طلبات اليوم',
-   value:' 25',
-   img:'assets/images/cr-1.PNG'
- },
- {
-  name:' اجمالي طلبات الاسبوع',
-  value:' 250',
-  img:'assets/images/cr-2.PNG'
-},
-{
- name:' اجمالي طلبات الشهر',
- value:' 0',
- img:'assets/images/cr-2.PNG'
-},
-{
-  name:' اجمالي الارباح الشهر',
-  value:' 0',
-  img:'assets/images/cr-4.PNG'
-},
-{
- name:'عدد طلبات الموبايل',
- value:' 25',
- img:'assets/images/cr-3.PNG'
-}
-  ]
+
+  private ApiService = inject(ApiService);
+
+  ngOnInit(): void {
+    this.getDashboardDetails();
+  }
+
+  items: any[] = [
+    { name: 'Client Count', value: 0, img: 'assets/images/dashboard/client.png', route: '/clients', id: 'clientCount' },
+    { name: 'Technical Count', value: 0, img: 'assets/images/dashboard/technical-support.png', route: '/technicals', id: 'technicalCount' },
+    { name: 'Order Count', value: 0, img: 'assets/images/dashboard/checklist.png', route: '/dashboard', id: 'orderCount' },
+    { name: 'Contract Type Count', value: 0, img: 'assets/images/dashboard/contract.png', route: '/contract-type', id: 'contractTypeCount' },
+    { name: 'Package Count', value: 0, img: 'assets/images/dashboard/package.png', route: '/package', id: 'packageCount' },
+    { name: 'Payment Way Count', value: 0, img: 'assets/images/dashboard/payment-method.png', route: '/paymentWay', id: 'paymentWayCount' },
+    { name: 'Service Count', value: 0, img: 'assets/images/dashboard/customer-service.png', route: '/services', id: 'serviceCount' },
+    { name: 'Working Time Count', value: 0, img: 'assets/images/dashboard/timetable.png', route: '/working_hours', id: 'workingTimeCount' },
+    { name: 'Terms and Conditions Count', value: 0, img: 'assets/images/dashboard/terms-and-conditions.png', route: '/settings/terms_conditions', id: 'termsAndConditionsCount' },
+    { name: 'Technical Specialist Count', value: 0, img: 'assets/images/dashboard/public-relations.png', route: '', id: 'technicalSpecialistCount' },
+    { name: 'Order Additional Items Count', value: 0, img: 'assets/images/dashboard/checklist.png', route: '/dashboard', id: 'orderAdditionalItemsCount' },
+    { name: 'FAQs Count', value: 0, img: 'assets/images/dashboard/faq.png', route: '/settings/faqs', id: 'faQsCount' },
+    { name: 'Country Count', value: 0, img: 'assets/images/dashboard/coronavirus.png', route: '/country', id: 'countryCount' },
+    { name: 'Coupon Count', value: 0, img: 'assets/images/dashboard/coupons.png', route: '/copone', id: 'coponeCount' },
+    { name: 'Complaint Count', value: 0, img: 'assets/images/dashboard/bad.png', route: '/complaint', id: 'complaintCount' },
+    { name: 'City Count', value: 0, img: 'assets/images/dashboard/cityscape.png', route: '/city', id: 'cityCount' },
+    { name: 'Cancel Reason Count', value: 0, img: 'assets/images/dashboard/no-data.png', route: '/cancel-reason', id: 'cancelReasonCount' }
+  ];
+
+
+
+
+  getDashboardDetails() {
+    this.ApiService.post('Dashborad/GetAll', {}).subscribe((res: any) => {
+      console.log(res);
+      this.updateItemsWithData(res.data);
+
+    })
+  }
+  updateItemsWithData(data: any) {
+    this.items.forEach((item: any) => {
+      if (data.hasOwnProperty(item.id)) {
+        item.value = data[item.id];
+      }
+    });
+  }
+
 }
