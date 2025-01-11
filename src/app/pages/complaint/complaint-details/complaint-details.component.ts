e4b1b17b36b8f9bd648c01fe27864df74063c70a
ahmedaladdin6 <ahmedaladdin6@gmail.com>
2025-01-11 11:23:39 +0200

    working on complaint translation

--- src/app/pages/complaint/complaint-details/complaint-details.component.ts
+++ src/app/pages/complaint/complaint-details/complaint-details.component.ts
@@ -12,17 +12,19 @@ import { IBreadcrumb } from '../../../components/breadcrump/cerqel-breadcrumb.in
 import { ConfirmMsgService } from '../../../services/confirm-msg.service';
 import { DialogComponent } from '../../../components/dialog/dialog.component';
 import { UploadFileComponent } from "../../../components/upload-file/upload-file.component";
+import { TranslatePipe } from '@ngx-translate/core';
+import { LanguageService } from '../../../services/language.service';
 
-const global_PageName='complaint';
-const global_API_deialis=global_PageName+'/GetById';
-const global_API_create=global_PageName+'/Create';
-const global_API_update=global_PageName+'/Update';
-const global_routeUrl =global_PageName
+const global_PageName='complaint.pageName';
+const global_API_deialis='complaint'+'/GetById';
+const global_API_create='complaint'+'/Create';
+const global_API_update='complaint'+'/Update';
+const global_routeUrl ='complaint'
 
 @Component({
   selector: 'app-complaint-details',
   standalone: true,
-  imports: [ReactiveFormsModule,TitleCasePipe, ButtonModule, NgIf, DialogComponent, InputTextComponent, EditorComponent, RouterModule, BreadcrumpComponent, UploadFileComponent],
+  imports: [ReactiveFormsModule,TitleCasePipe,TranslatePipe, ButtonModule, NgIf, DialogComponent, InputTextComponent, EditorComponent, RouterModule, BreadcrumpComponent, UploadFileComponent],
   templateUrl: './complaint-details.component.html',
   styleUrl: './complaint-details.component.scss'
 })
@@ -62,25 +64,41 @@ export class ComplaintDetailsComponent {
   })
 
   bredCrumb: IBreadcrumb = {
-    crumbs: [
-      {
-        label: 'Home',
-        routerLink: '/dashboard',
-      },
-      {
-        label: this.pageName(),
-      },
-    ]
+    crumbs: [ ]
   }
 
   get getID() {
     return this.route.snapshot.params['id']
   }
+  
+    selectedLang: any;
+    languageService = inject(LanguageService);
 
   ngOnInit() {
+    this.getBreadCrumb();
     this.pageName.set(global_PageName)
+
+    this.languageService.translationService.onLangChange.subscribe(() => {
+      this.selectedLang = this.languageService.translationService.currentLang;
+      this.getBreadCrumb();
+    });
     if (this.tyepMode() !== 'Add')
       this.API_getItemDetails()
+  }
+
+  getBreadCrumb() {
+    console.log('ffff',this.languageService.translate('complaint.pageName_View_crumb'))
+    this.bredCrumb = {
+      crumbs: [
+        {
+          label:  this.languageService.translate('Home'),
+          routerLink: '/dashboard',
+        },
+        {
+          label: this.languageService.translate('ببب'),
+        },
+      ]
+    }
   }
 
   tyepMode() {
