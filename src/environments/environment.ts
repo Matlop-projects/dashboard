cfa2899d734eade186b946d5446b8fe167e67975
aalaa <aalaa@youxel>
2025-04-23 22:40:11 +0200

    change URL

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
-    baseUrl: 'https://aspdev.matlop.com/api/',
+    baseUrl: 'https://asp.matlop.com/api/',
     // baseUrl: "https://abdoryad-001-site1.ktempurl.com/api/",
-     baseImageUrl: 'https://aspdev.matlop.com',
+     baseImageUrl: 'https://asp.matlop.com',
     // baseImageUrl: "https://abdoryad-001-site1.ktempurl.com"
   };
