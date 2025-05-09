a3f89ab361b276c9c0eddfead90f6746de9b43ff
aalaa <aalaa@youxel>
2025-05-10 01:42:52 +0300

    change url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
-    baseUrl: 'https://asp.matlop.com/api/',
+    baseUrl: 'https://backend.matlop.com/api/',
     // baseUrl: "https://abdoryad-001-site1.ktempurl.com/api/",
-     baseImageUrl: 'https://asp.matlop.com',
+     baseImageUrl: 'https://backend.matlop.com',
     // baseImageUrl: "https://abdoryad-001-site1.ktempurl.com"
   };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
-    baseUrl: 'https://asp.matlop.com/api/',
+    baseUrl: 'https://backend.matlop.com/api/',
     // baseUrl: "https://abdoryad-001-site1.ktempurl.com/api/",
-     baseImageUrl: 'https://asp.matlop.com',
+     baseImageUrl: 'https://backend.matlop.com',
     // baseImageUrl: "https://abdoryad-001-site1.ktempurl.com"
   };
