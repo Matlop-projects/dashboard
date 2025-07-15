0d689c2e9d54596ea4e69b2292cebb51d10cf160
ahmedaladdin6 <ahmedaladdin6@gmail.com>
2025-07-15 18:57:25 +0300

    done

--- src/app/pages/roles/role-details/role-details.component.ts
+++ src/app/pages/roles/role-details/role-details.component.ts
@@ -1,5 +1,11 @@
 import { Component, inject, signal } from '@angular/core';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
 import { ApiService } from '../../../services/api.service';
 import { ActivatedRoute, Router, RouterModule } from '@angular/router';
@@ -7,43 +13,57 @@ import { NgFor, NgIf, TitleCasePipe } from '@angular/common';
 import { Validations } from '../../../validations';
 import { InputTextComponent } from '../../../components/input-text/input-text.component';
 import { EditorComponent } from '../../../components/editor/editor.component';
-import { BreadcrumpComponent } from "../../../components/breadcrump/breadcrump.component";
+import { BreadcrumpComponent } from '../../../components/breadcrump/breadcrump.component';
 import { IBreadcrumb } from '../../../components/breadcrump/cerqel-breadcrumb.interface';
 import { ConfirmMsgService } from '../../../services/confirm-msg.service';
 import { DialogComponent } from '../../../components/dialog/dialog.component';
-import { UploadFileComponent } from "../../../components/upload-file/upload-file.component";
+import { UploadFileComponent } from '../../../components/upload-file/upload-file.component';
 import { ToggleSwitchModule } from 'primeng/toggleswitch';
 import { Checkbox } from 'primeng/checkbox';
 import { TranslatePipe } from '@ngx-translate/core';
-import { CheckBoxComponent } from "../../../components/check-box/check-box.component";
+import { CheckBoxComponent } from '../../../components/check-box/check-box.component';
 import { LanguageService } from '../../../services/language.service';
 
-
-
-const global_PageName='roles.pageName';
-const global_API_deialis='Role'+'/GetById';
-const global_API_create='Role'+'/Create';
-const global_API_update='Role'+'/Update';
-const global_routeUrl ='/settings/roles'
+const global_PageName = 'roles.pageName';
+const global_API_deialis = 'Role' + '/GetById';
+const global_API_create = 'Role' + '/Create';
+const global_API_update = 'Role' + '/Update';
+const global_routeUrl = '/settings/roles';
 @Component({
   selector: 'app-role-details',
   standalone: true,
-  imports: [ReactiveFormsModule, TranslatePipe, ToggleSwitchModule, NgFor, Checkbox, FormsModule, TitleCasePipe, ButtonModule, NgIf, DialogComponent, InputTextComponent, EditorComponent, RouterModule, BreadcrumpComponent, UploadFileComponent, CheckBoxComponent],
+  imports: [
+    ReactiveFormsModule,
+    TranslatePipe,
+    ToggleSwitchModule,
+    NgFor,
+    Checkbox,
+    FormsModule,
+    TitleCasePipe,
+    ButtonModule,
+    NgIf,
+    DialogComponent,
+    InputTextComponent,
+    EditorComponent,
+    RouterModule,
+    BreadcrumpComponent,
+    UploadFileComponent,
+    CheckBoxComponent,
+  ],
   templateUrl: './role-details.component.html',
-  styleUrl: './role-details.component.scss'
+  styleUrl: './role-details.component.scss',
 })
 export class RoleDetailsComponent {
-
-  pageName =signal<string>(global_PageName);
-  private ApiService = inject(ApiService)
-  private router = inject(Router)
-  private route = inject(ActivatedRoute)
-  showConfirmMessage: boolean = false
-  private confirm = inject(ConfirmMsgService)
-  rolesList:any[]=[]
-  selcectAllRolesValue=false
-  selectedRoles:any=[]
-  convertSelectedRoles:any=[]
+  pageName = signal<string>(global_PageName);
+  private ApiService = inject(ApiService);
+  private router = inject(Router);
+  private route = inject(ActivatedRoute);
+  showConfirmMessage: boolean = false;
+  private confirm = inject(ConfirmMsgService);
+  rolesList: any[] = [];
+  selcectAllRolesValue = false;
+  selectedRoles: any = [];
+  convertSelectedRoles: any = [];
   form = new FormGroup({
     enName: new FormControl('', {
       validators: [
@@ -54,74 +74,76 @@ export class RoleDetailsComponent {
     arName: new FormControl('', {
       validators: [
         Validators.required,
-        Validations.arabicCharsValidator('isArabic')
-      ]
+        Validations.arabicCharsValidator('isArabic'),
+      ],
     }),
-    roleId:new FormControl(this.getID|0),
+    roleId: new FormControl(this.getID | 0),
     accessList: new FormControl('string'),
-  })
+  });
 
   bredCrumb: IBreadcrumb = {
-    crumbs: []
-  }
+    crumbs: [],
+  };
   checkToggle: boolean = true;
 
   get getID() {
-    return this.route.snapshot.params['id']
+    return this.route.snapshot.params['id'];
   }
 
   selectedLang: any;
   languageService = inject(LanguageService);
-  
+
   ngOnInit() {
-    this.getAllControllersActions()
-    this.pageName.set(global_PageName)
+    this.getAllControllersActions();
+
+    if (this.tyepMode() !== 'Add') this.API_getItemDetails();
+    this.pageName.set(global_PageName);
     this.getBreadCrumb();
     this.languageService.translationService.onLangChange.subscribe(() => {
       this.selectedLang = this.languageService.translationService.currentLang;
       this.getBreadCrumb();
-      this.getAllControllersActions()
-      if (this.tyepMode() !== 'Add')
-          this.API_getItemDetails()
+      this.getAllControllersActions();
+
+      if (this.tyepMode() !== 'Add') this.API_getItemDetails();
     });
-    if (this.tyepMode() !== 'Add')
-      this.API_getItemDetails()
   }
 
   tyepMode() {
     const url = this.router.url;
-    let result = 'Add'
-    if (url.includes('edit')) result = 'Edit'
-    else if (url.includes('view')) result = 'View'
-    else result = 'Add'
-    return result
+    let result = 'Add';
+    if (url.includes('edit')) result = 'Edit';
+    else if (url.includes('view')) result = 'View';
+    else result = 'Add';
+    return result;
   }
   getBreadCrumb() {
     this.bredCrumb = {
       crumbs: [
         {
-          label:  this.languageService.translate('Home'),
+          label: this.languageService.translate('Home'),
           routerLink: '/dashboard',
         },
         {
-          label: this.languageService.translate(this.pageName()+ '_'+this.tyepMode()+'_crumb'),
+          label: this.languageService.translate(
+            this.pageName() + '_' + this.tyepMode() + '_crumb'
+          ),
         },
-      ]
-    }
+      ],
+    };
   }
- onToggleAll(checked: boolean) {
-  this.selcectAllRolesValue = checked;
+  onToggleAll(checked: boolean) {
+    this.selcectAllRolesValue = checked;
 
-  this.rolesList.forEach((controller, index) => {
-    controller.actions.forEach((action: any) => {
-      action.checked = checked;
+    this.rolesList.forEach((controller, index) => {
+      controller.actions.forEach((action: any) => {
+        action.checked = checked;
+      });
     });
-  });
 
-  const filtered = this.filterCheckedActions(this.rolesList);
-  this.convertSelectedRoles = [...filtered];
-  this.selectedRoles = [...filtered];
-}
+    const filtered = this.filterCheckedActions(this.rolesList);
+    this.convertSelectedRoles = [...filtered];
+    this.selectedRoles = [...filtered];
+  }
 
   // onToggle(checked:boolean,controller:any,action:any,index:number){
   //   if(checked){
@@ -144,172 +166,181 @@ export class RoleDetailsComponent {
   // this.selcectAllRolesValue = this.isAllTogglesChecked(this.rolesList);
 
   // }
-  onToggle(checked: boolean, controllerName: string, actionName: string, index: number) {
-  // Update the rolesList directly
-  const controller = this.rolesList.find(c => c.controller === controllerName);
-  if (controller) {
-    const action = controller.actions.find((a: any) => a.name === actionName);
-    if (action) {
-      action.checked = checked;
+  onToggle(
+    checked: boolean,
+    controllerName: string,
+    actionName: string,
+    index: number
+  ) {
+    // Update the rolesList directly
+    const controller = this.rolesList.find(
+      (c) => c.controller === controllerName
+    );
+    if (controller) {
+      const action = controller.actions.find((a: any) => a.name === actionName);
+      if (action) {
+        action.checked = checked;
+      }
     }
+
+    // Update selected roles arrays
+    this.convertSelectedRoles = this.filterCheckedActions(this.rolesList);
+    this.selectedRoles = this.filterCheckedActions(this.rolesList);
+
+    // Recalculate "Select All" checkbox state
+    this.selcectAllRolesValue = this.isAllTogglesChecked(this.rolesList);
   }
-
-  // Update selected roles arrays
-  this.convertSelectedRoles = this.filterCheckedActions(this.rolesList);
-  this.selectedRoles = this.filterCheckedActions(this.rolesList);
-
-  // Recalculate "Select All" checkbox state
-  this.selcectAllRolesValue = this.isAllTogglesChecked(this.rolesList);
-}
 
   API_getItemDetails() {
-    this.ApiService.get(`${global_API_deialis}/${this.getID}`).subscribe((res: any) => {
-      if (res)
-        this.form.patchValue({
-          accessList:res.data.accessList,
-          roleId:res.data.roleId,
-          arName:res.data.arName,
-          enName:res.data.enName,
-
-        })
-        this.updateCheckedStatus(res.data.controllerInfo,this.rolesList)
-    },()=>{},()=>{
-      this.selcectAllRolesValue= this.isAllTogglesChecked(this.rolesList)
-    })
-  }
-  updateCheckedStatus(rolesFromDetails:any[], rolesList:any[]) {
-    rolesList.forEach(controllerObj => {
-        const firstController = rolesFromDetails.find(controller => controller.controller === controllerObj.controller);
-      
-        if (firstController) {
-            controllerObj.actions.forEach((action:any) => {
-                if (firstController.actions.includes(action.name)) {
-                    action.checked = true;
-                }
-            });
-        }
-    });
-      this.convertSelectedRoles =this.filterCheckedActions(rolesList)
-    return rolesList
-
-}
-
-filterCheckedActions(array:any) {
-  let result:any = [];
-
-  array.forEach((item:any, index:number) => {
-      item.actions.forEach((action:any) => {
-          if (action.checked) {
-              result.push({
-                  id: index,
-                  controller: item.controller,
-                  actions: [action]
-              });
-          }
-      });
-  });
-
-  return result;
-}
-
-  getAllControllersActions(){
-    this.ApiService.get('Role/controllers-actions').subscribe((res:any) => {
-      this.rolesList=[]
-      if(res){
-        
-        res.map((role:any,index:number)=>{
-          let action:any =[]
-           role.actions.map((ac:string)=>{
-                action.push({
-                  name:ac,
-                  checked:false
-                })
-              
-           })
-            this.rolesList[index]={
-            actions:action,
-            controller:role.controller
-           }
-        })
+    this.ApiService.get(`${global_API_deialis}/${this.getID}`).subscribe(
+      (res: any) => {
+        if (res)
+          this.form.patchValue({
+            accessList: res.data.accessList,
+            roleId: res.data.roleId,
+            arName: res.data.arName,
+            enName: res.data.enName,
+          });
+        this.updateCheckedStatus(res.data.controllerInfo, this.rolesList);
+      },
+      () => {},
+      () => {
+        this.selcectAllRolesValue = this.isAllTogglesChecked(this.rolesList);
       }
-    })
+    );
   }
-isAllTogglesChecked(array:any): boolean {
-  for (const controller of array) {
-    for (const action of controller.actions) {
-      if (!action.checked) return false;
+  updateCheckedStatus(rolesFromDetails: any[], rolesList: any[]) {
+    rolesList.forEach((controllerObj) => {
+      const firstController = rolesFromDetails.find(
+        (controller) => controller.controller === controllerObj.controller
+      );
+
+      if (firstController) {
+        controllerObj.actions.forEach((action: any) => {
+          if (firstController.actions.includes(action.name)) {
+            action.checked = true;
+          }
+        });
+      }
+    });
+    this.convertSelectedRoles = this.filterCheckedActions(rolesList);
+    return rolesList;
+  }
+
+  filterCheckedActions(array: any) {
+    let result: any = [];
+
+    array.forEach((item: any, index: number) => {
+      item.actions.forEach((action: any) => {
+        if (action.checked) {
+          result.push({
+            id: index,
+            controller: item.controller,
+            actions: [action],
+          });
+        }
+      });
+    });
+
+    return result;
+  }
+
+  getAllControllersActions() {
+    this.ApiService.get('Role/controllers-actions').subscribe((res: any) => {
+      this.rolesList = [];
+      if (res) {
+        res.map((role: any, index: number) => {
+          let action: any = [];
+          role.actions.map((ac: string) => {
+            action.push({
+              name: ac,
+              checked: false,
+            });
+          });
+          this.rolesList[index] = {
+            actions: action,
+            controller: role.controller,
+          };
+        });
+      }
+    });
+  }
+  isAllTogglesChecked(array: any): boolean {
+    for (const controller of array) {
+      for (const action of controller.actions) {
+        if (!action.checked) return false;
+      }
     }
+    return true;
   }
-  return true;
-}
   onSubmit() {
     const payload = {
       ...this.form.value,
-      controllerInfo:this.tyepMode() == 'Add'?this.setRolesToSubmit(this.selectedRoles):this.simplifyActions(this.setRolesToSubmit(this.convertSelectedRoles))
-    }
+      controllerInfo:
+        this.tyepMode() == 'Add'
+          ? this.setRolesToSubmit(this.selectedRoles)
+          : this.simplifyActions(
+              this.setRolesToSubmit(this.convertSelectedRoles)
+            ),
+    };
 
-    if (this.tyepMode() == 'Add')
-      this.API_forAddItem(payload)
-    else
-      this.API_forEditItem(payload)
+    if (this.tyepMode() == 'Add') this.API_forAddItem(payload);
+    else this.API_forEditItem(payload);
+  }
 
-   }
-
-   setRolesToSubmit(arraySelected:any){
-   const transformed:any = Object.values(
-      (arraySelected as any[]).reduce((acc:any, { controller, actions }) => {
-          if (!acc[controller]) {
-              acc[controller] = { controller, actions: [] };
-          }
-          acc[controller].actions = [...new Set([...acc[controller].actions, ...actions])];
-          return acc;
+  setRolesToSubmit(arraySelected: any) {
+    const transformed: any = Object.values(
+      (arraySelected as any[]).reduce((acc: any, { controller, actions }) => {
+        if (!acc[controller]) {
+          acc[controller] = { controller, actions: [] };
+        }
+        acc[controller].actions = [
+          ...new Set([...acc[controller].actions, ...actions]),
+        ];
+        return acc;
       }, {})
-  );
+    );
 
-    return transformed
-   }
+    return transformed;
+  }
 
-  simplifyActions(data:any[]) {
-    return data.map(item => ({
-        id: item.id,
-        controller: item.controller,
-        actions: item.actions.map((action:any) => action.name) 
+  simplifyActions(data: any[]) {
+    return data.map((item) => ({
+      id: item.id,
+      controller: item.controller,
+      actions: item.actions.map((action: any) => action.name),
     }));
-}
-  navigateToPageTable(){
-    this.router.navigateByUrl(global_routeUrl)
+  }
+  navigateToPageTable() {
+    this.router.navigateByUrl(global_routeUrl);
   }
 
   cancel() {
-    const hasValue = this.confirm.formHasValue(this.form)
-    if (hasValue && this.tyepMode()=='Edit')
-      this.showConfirmMessage = !this.showConfirmMessage
-    else
-      this.navigateToPageTable()
-
+    const hasValue = this.confirm.formHasValue(this.form);
+    if (hasValue && this.tyepMode() == 'Edit')
+      this.showConfirmMessage = !this.showConfirmMessage;
+    else this.navigateToPageTable();
   }
 
   onConfirmMessage() {
-    this.navigateToPageTable()
-
+    this.navigateToPageTable();
   }
 
-
   API_forAddItem(payload: any) {
-
-    this.ApiService.post(global_API_create, payload, { showAlert: true, message: `Add ${this.pageName()} Successfuly` }).subscribe(res => {
-      if (res)
-        this.navigateToPageTable()
-    })
+    this.ApiService.post(global_API_create, payload, {
+      showAlert: true,
+      message: `Add ${this.pageName()} Successfuly`,
+    }).subscribe((res) => {
+      if (res) this.navigateToPageTable();
+    });
   }
 
   API_forEditItem(payload: any) {
-
-    this.ApiService.put(global_API_update, payload, { showAlert: true, message: `update ${this.pageName()} Successfuly` }).subscribe(res => {
-      if (res)
-        this.navigateToPageTable()
-    })
+    this.ApiService.put(global_API_update, payload, {
+      showAlert: true,
+      message: `update ${this.pageName()} Successfuly`,
+    }).subscribe((res) => {
+      if (res) this.navigateToPageTable();
+    });
   }
-
-
 }
