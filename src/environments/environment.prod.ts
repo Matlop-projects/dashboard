d0b249970ac69663f39fcbc5728a4c944d6d6c1d
Youssef Amr <yamrrushdy@gmail.com>
2025-11-16 20:52:24 +0200

    .

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    baseUrl: 'http://localhost:5202/api/',
+    baseUrl: 'https://backend.matlop.com/api/',
     // baseUrl: "https://abdoryad-001-site1.ktempurl.com/api/",
      baseImageUrl: 'https://backend.matlop.com',
     // baseImageUrl: "https://abdoryad-001-site1.ktempurl.com"
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,6 @@
 export const environment = {
     production: false,
-    baseUrl: 'http://localhost:5202/api/',
-    // baseUrl: "https://abdoryad-001-site1.ktempurl.com/api/",
+    baseUrl: 'https://backend.matlop.com/api/',    // baseUrl: "https://abdoryad-001-site1.ktempurl.com/api/",
      baseImageUrl: 'https://backend.matlop.com',
     // baseImageUrl: "https://abdoryad-001-site1.ktempurl.com"
   };
