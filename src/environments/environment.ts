0965fccf56513bf554d77dcb2386d7554581ae2f
Youssef Amr <yamrrushdy@gmail.com>
2026-03-08 12:25:09 +0200

    .

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    baseUrl: 'https://localhost:3040/api/',    // baseUrl: "https://abdoryad-001-site1.ktempurl.com/api/",
+    baseUrl: 'https://test.wrdah.com/api/',    // baseUrl: "https://abdoryad-001-site1.ktempurl.com/api/",
      baseImageUrl: 'https://backend.matlop.com',
     // baseImageUrl: "https://abdoryad-001-site1.ktempurl.com"
   };
